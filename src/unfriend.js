783284488190777aed26207fa990c6e8af0415b1
lequanglam <lqlam2004@gmail.com>
2020-01-12 18:07:13 +0700

    unfriend

---
+++ src/unfriend.js
@@ -0,0 +1,38 @@
+"use strict";
+
+var utils = require("../utils");
+var log = require("npmlog");
+
+module.exports = function(defaultFuncs, api, ctx) {
+  return function unsendMessage(userID, callback) {
+    if (!callback) {
+      callback = function() {};
+    }
+
+    var form = {
+      uid: userID,
+      unref: "bd_friends_tab",
+      floc: "friends_tab",
+      "nctr[_mod]": "pagelet_timeline_app_collection_" + ctx.userID + ":2356318349:2"
+    };
+
+    defaultFuncs
+      .post(
+        "https://www.facebook.com/ajax/profile/removefriendconfirm.php",
+        ctx.jar,
+        form
+      )
+      .then(utils.parseAndCheckLogin(ctx, defaultFuncs))
+      .then(function(resData) {
+        if (resData.error) {
+          throw resData;
+        }
+
+        return callback();
+      })
+      .catch(function(err) {
+        log.error("unfriend", err);
+        return callback(err);
+      });
+  };
+};
